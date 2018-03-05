edaa09a76ebdf72bc766b21d321c4b317ddb0ec4
XiaoYaoNet <XiaoYaoNet@test.com>
2018-03-05 18:18:46 +0800

    USBread

---
+++ USBread/Uread/Uread/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace Uread
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(170, 48);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(177, 21);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(65, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "设置拷贝路径";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(360, 171);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ USBread/Uread/Uread/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace Uread
+{
+    public partial class Form1 : Form
+    {
+        string PoliceID = "";
+        string DeviceModel = "";
+        private List<string> _serialNumber = new List<string>();
+        public const int WM_DEVICECHANGE = 0x219;
+        public const int DBT_DEVICEARRIVAL = 0x8000;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void WndProc(ref   Message m)
+        {
+            try
+            {
+                if (m.Msg == WM_DEVICECHANGE)
+                {
+                    switch (m.WParam.ToInt32())
+                    {
+                        case DBT_DEVICEARRIVAL: //U盘插入   
+                            DriveInfo[] s = DriveInfo.GetDrives();
+                            foreach (DriveInfo drive in s)
+                            {
+                                if (drive.DriveType == DriveType.Removable)
+                                {
+                                    DeviceModel = drive.Name.ToString();
+                                    FileSend();
+                                    break;
+                                }
+                            }
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            base.WndProc(ref   m);
+        }
+
+        private void FileSend()
+        {                                                                                                    //源代码
+            CopyDirectory(DeviceModel, textBox1.Text.ToString());                       //源代码
+
+        }
+        private void CopyDirectory(string srcdir, string desdir)
+        {
+            string folderName = srcdir.Substring(srcdir.LastIndexOf("\\") + 1);
+
+            string desfolderdir = desdir + "\\" + PoliceID;
+
+            if (desdir.LastIndexOf("\\") == (desdir.Length - 1))
+            {
+                desfolderdir = desdir + folderName;
+            }
+            string[] filenames = Directory.GetFileSystemEntries(srcdir);
+
+            foreach (string file in filenames)// 遍历所有的文件和目录
+            {
+                if (Directory.Exists(file))// 先当作目录处理如果存在这个目录就递归Copy该目录下面的文件
+                {
+
+                    string currentdir = desfolderdir + "\\" + file.Substring(file.LastIndexOf("\\") + 1);
+                    if (!Directory.Exists(currentdir))
+                    {
+                        Directory.CreateDirectory(currentdir);
+                    }
+
+                    CopyDirectory(file, desfolderdir);
+                }
+
+                else // 否则直接copy文件
+                {
+                    string srcfileName = file.Substring(file.LastIndexOf("\\") + 1);
+
+                    srcfileName = desfolderdir + "\\" + srcfileName;
+
+
+                    if (!Directory.Exists(desfolderdir))
+                    {
+                        Directory.CreateDirectory(desfolderdir);
+                    }
+
+
+                    try
+                    {
+                        File.Copy(file, srcfileName);
+                    }
+                    catch (Exception e)
+                    {
+                        continue;
+                    }
+
+                }
+            }//foreach 
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
